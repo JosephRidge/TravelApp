e5e2b4afb1f20f451657b71ffd8cd193fb6b020b
JosephRidge <josephridge18@gmail.com>
2025-08-06 12:29:55 +0300

    update

--- lib/pages/landing/landing.dart
+++ lib/pages/landing/landing.dart
@@ -1,3 +1,5 @@
+import 'dart:ui' as ui;
+
 import 'package:flutter/material.dart';
 
 class LandingPage extends StatefulWidget {
@@ -10,6 +12,46 @@ class LandingPage extends StatefulWidget {
 class _LandingPageState extends State<LandingPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Stack(
+      // layout widget
+      alignment: AlignmentDirectional.bottomEnd,
+      children: [
+        // adding the image background
+        Positioned.fill(
+          child: Image(
+            image: AssetImage("assets/images/islands.jpg"),
+            fit: BoxFit.cover,
+          ),
+        ),
+
+        //bottom blurred out section
+        ClipRRect(
+          // utility widget
+          child: BackdropFilter(
+            // glasslike feature
+            filter: ui.ImageFilter.blur(sigmaX: 15.0, sigmaY: 15.0),
+            child: SizedBox(
+              height: 300,
+              width: double.infinity,
+              child: Column(
+                children: [
+                  // rich text
+                  RichText(
+                    textAlign: TextAlign.center,
+                    text: TextSpan(
+                      text: 'Unveil The Travel Wonders',
+                      style: TextStyle(
+                        color: const ui.Color.fromARGB(255, 255, 255, 255),
+                        fontSize: 40,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
